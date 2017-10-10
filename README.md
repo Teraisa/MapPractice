55f78aa04f01d87de6b112411a51441ff1d97aa2
Teraisa <Teraisa@gmail.com>
2017-10-09 17:09:27 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Teraisa's Neighborhood Map Project:
+_by Teraisa J. Rogers_ with Udacity
+
+
+With the help of Knockout JS, Google Maps API, Foursquare API, jQUERY, Bootstrap, Javascript, CSS, and HTML, we've compiled a list of five assorted true crimes along with their location.
+
+### Before you begin (AKA Special Needs and Requirements)
+1. Clone or download this repo
+
+### What Now?
+*Open `index.html` in your browser, the map will load automatically. Feel free to explore.
+
+--------------
+
+MIT Licensing; ©Teraisa 2017
+
+### Still here?
+>This README is in *"plain"* English for a reason
+(always looking out for the very young coders)
+
+#neighborHoodMap
---
+++ js/data.js
@@ -0,0 +1,46 @@
+/* True Crime Location Data */
+
+var locations = [
+    {
+        title: "Tupac",
+        location: {
+            lat: 36.114479,
+            lng: -115.164013
+        }
+    },
+    {
+        title: "Sadie Natasha Jennings",
+        location: {
+            lat: 39.163798,
+            lng: -119.767403
+        }
+    },
+    {
+        title: "Ricardo Martinez",
+        location: {
+            lat: 39.252564,
+            lng: -119.944418
+        }
+    },
+    {
+        title: "Monica DaSilva",
+        location: {
+            lat: 39.513215,
+            lng: -119.848012
+        }
+    },
+    {
+        title: "Charles and Nona Rauer",
+        location: {
+            lat: 39.249683,
+            lng: -119.952685
+        }
+    },
+    {
+        title: "Albert and Joan Musalo",
+        location: {
+            lat: 39.347074,
+            lng: -119.848814
+        }
+    }
+];
